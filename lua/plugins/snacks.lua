56ff0e27db0bb6bc2d7363e90f0e81e79974e39c
SailorSnoW <sailorsnow@pm.me>
2025-02-18 20:40:25 +0100

    add snacks.image and switch to relative number

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -287,6 +287,7 @@ return {
     explorer = { enabled = true },
     indent = { enabled = true },
     input = { enabled = true },
+    image = { enabled = true },
     picker = { enabled = true },
     notifier = { enabled = true },
     quickfile = { enabled = true },
